970434362e60047c7815a55110877272f142e15b
Frederic <frederic@askarel.be>
2012-10-02 23:19:32 +0200

    - Can now remove 1 scanned item - some rework to allow checking against multiple CSV files

--- zoppas/zoppasfs/mkinit.sh
+++ zoppas/zoppasfs/mkinit.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SERVERPUSH="root@gate:/srv/tftp/zoppas/"
+
 if [ "$(pwd)" = "/" ]; then
     echo "I will not read from there !!"
     exit 1
@@ -7,3 +9,7 @@ fi
 
 INITRD=$(basename $(pwd))
 sync; find . | cpio -o -H newc | gzip -9 > ../$INITRD.gz
+
+test -n "$SERVERPUSH" && scp "../$INITRD.gz" "$SERVERPUSH"
+
+
