074f3909b6b32e20d78085b3ae446a00adce1547
frederic <frederic@askarel.be>
2014-03-23 20:53:38 +0100

    pidfile.pas - forgot license infos

--- io/pidfile.pas
+++ io/pidfile.pas
@@ -1,3 +1,23 @@
+{
+ pidfile.pas - routines to handle the PID file
+
+ (c) 2014 Frederic Pasteleurs <frederic@askarel.be>
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program or from the site that you downloaded it
+ from; if not, see <http://www.gnu.org/licenses/>.
+}
+
 unit pidfile;
 interface
 
