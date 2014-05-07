882d724bd302cb1a5bb6e1b9c07236a8580822ea
frederic <frederic@askarel.be>
2014-05-07 12:02:39 +0200

    - Added wrapper script to illustrate the call-on-log feature of the controller

---
+++ io/blackknightio.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Sample script to illustrate the calling feature of the door controller
+
+ME="$(basename $0)"
+
+logger -t $ME "$# Command line parameter received: '$1' '$2' '$3'"
+
+test -x "$(which flite)" && flite -t "$2"
