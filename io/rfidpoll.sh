8863451a2a96d06bd4b8df4951cf2ce40081e1a3
frederic <frederic@askarel.be>
2014-05-25 03:34:53 +0200

    Added possibility to block the script while updating the database

--- io/rfidpoll.sh
+++ io/rfidpoll.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 ME=$(basename $0)
+PAUSEFILE="/tmp/$ME.register"
 #set -x
 
+# Kill pause file
+rm -f $PAUSEFILE
 test -z "$(which scriptor)" && echo "Scriptor not found !" && exit 1
 
 while true; do
- ATRHASH=$(echo "reset" | scriptor 2> /dev/null | tail -n 1 | md5sum | cut -d ' ' -f 1)
- if [ "$ATRHASH" != 'd41d8cd98f00b204e9800998ecf8427e' ] ; then # d41d8cd98f00b204e9800998ecf8427e is the empty string hash
-  if [ "$CARDHASH" = '' ]; then
+ if [ -f "$PAUSEFILE" ]; then
+  logger -t $ME "Paused: about to add a user to the database"
+  sleep 1
+ else
+  ATRHASH=$(echo "reset" | scriptor 2> /dev/null | tail -n 1 | md5sum | cut -d ' ' -f 1)
+  if [ "$ATRHASH" != 'd41d8cd98f00b204e9800998ecf8427e' ] ; then # d41d8cd98f00b204e9800998ecf8427e is the empty string hash
+   if [ "$CARDHASH" = '' ]; then
+    # Ask the card UID and hash the result (i don't care about the garbage)
     CARDHASH=$( (echo 'ffca000000'|scriptor 2> /dev/null|tail -n 1 | md5sum | cut -d ' ' -f 1; echo " $ATRHASH") | md5sum |cut -d ' ' -f 1)
     logger -t "$ME" "Card scanned: hash: $CARDHASH"
 
@@ -20,12 +28,12 @@ while true; do
     else
      logger -t $ME "WARNING: UNKNOWN TAG: $CARDHASH"
     fi
+   fi
+  else
+   unset CARDHASH
   fi
- else
-  unset CARDHASH
+  unset ATRHASH
+  sleep 0.5
  fi
-
- unset ATRHASH
- sleep 0.5
 
 done
