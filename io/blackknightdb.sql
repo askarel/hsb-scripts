0606f29068e7dd24dfa24f7e57a639cae3b054c9
frederic <frederic@askarel.be>
2014-08-26 12:27:54 +0200

    Added more revocation options

--- io/blackknightdb.sql
+++ io/blackknightdb.sql
@@ -123,7 +123,7 @@ CREATE TABLE `logs_door` (
   DELIMITER $$
   create procedure rfid_db_hsbxl.getflattags ()
   BEGIN
-    select uid, unix_timestamp (validitystart), unix_timestamp (validityend), 0, 0, users_vs_tags.user_login from tags, users_vs_tags where uid=tag_uid;
+    select uid, unix_timestamp (validitystart), unix_timestamp (validityend), 0, 0, users_vs_tags.user_login from tags, users_vs_tags, tags_status where (uid=tag_uid) and tags_status.status_is_valid;
   END;
   $$
   DELIMITER ;
