c2df75f75213f23261ea273c4932211c1bd9a6e9
frederic <frederic@askarel.be>
2014-09-12 09:39:43 +0200

    learning about sessions

--- concierge/lib/login.php
+++ concierge/lib/login.php
@@ -27,44 +27,37 @@ if ( !isset ($CONFIGFILE))
     exit;
 }
 
+$MissingPwText = '';
+$MissingUsrText = '';
+
 # Is there a user name ?
-if (isset ($_POST['hsbuser']))
+if (isset ($SANITIZED_POST['hsbuser']) and ($SANITIZED_POST['hsbuser'] == ''))
     {
-	if ($_POST['hsbuser'] == '')
-	    {
-		$MissingUsrText = '<font color="red">Username is required</font>';
-	    }
-    } else
-    {
-	$MissingUsrText = '';
-    }
+	$MissingUsrText = '<font color="red">Username is required</font>';
+    } 
 
 # Is there a password ?
-if (isset ($_POST['hsbpass']))
+if (isset ($SANITIZED_POST['hsbpass']) and ($SANITIZED_POST['hsbpass'] == '') and (!isset ($SANITIZED_POST['lostpw'])))
     {
-	if (($_POST['hsbpass'] == '') and (!isset ($_POST['lostpw'])))
-	    {
-		$MissingPwText = '<font color="red">Password is required</font>';
-	    }
-    } else
-    {
-	$MissingPwText = '';
+	$MissingPwText = '<font color="red">Password is required</font>';
     }
 
 # Process password reset (TODO)
 
-# Check password and set cookie (TODO)
+# Check password and set session variable (TODO)
 
-
+# Show login form
 html_header ('Login');
 printf (" <H1>%s Members Login Form</H1>\n", $CONFIG['orgname']);
 printf (" <FORM Method=\"POST\" Action=\"%s\">\n", $_SERVER['SCRIPT_NAME']);
-printf ("  Username: <INPUT type=\"text\" size=20 name=\"hsbuser\" value=\"%s\">%s<br />\n", $_POST['hsbuser'], $MissingUsrText);
+printf ("  Username: <INPUT type=\"text\" size=20 name=\"hsbuser\" value=\"%s\">%s<br />\n", $SANITIZED_POST['hsbuser'], $MissingUsrText);
 printf ("  Password: <INPUT type=\"password\" size=20 name=\"hsbpass\">%s<br />\n", $MissingPwText);
 printf (" <INPUT type=\"submit\" value=\"Login\">\n");
 printf (" <INPUT TYPE=\"submit\" name=\"lostpw\" value=\"lost password ?\"><br />");
 printf (" <INPUT TYPE=\"submit\" name=\"newmember\" value=\"Become a member here\"><br />");
 printf (" </FORM>\n");
+
+#printf ("%s<br />\n", password_hash ($SANITIZED_POST['hsbpass'], PASSWORD_DEFAULT) );
 #dumparray ($_SERVER, '$_SERVER');
-dumparray ($_POST, '$_POST');
+dumparray ($SANITIZED_POST, '$SANITIZED_POST');
 ?>
