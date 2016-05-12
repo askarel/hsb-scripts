ab28d9cfa6ade47e2be47691dc8d7886dd343f24
frederic <frederic@askarel.be>
2016-05-12 22:12:54 +0200

    More data logged from the history trigger: add expiry date

--- concierge/sql/triggers.sql
+++ concierge/sql/triggers.sql
@@ -21,14 +21,14 @@ DELIMITER ;;
 DROP TRIGGER if exists log_person_activity_update;;
 CREATE TRIGGER log_person_activity_update AFTER UPDATE ON person FOR EACH ROW
     begin
-	if new.machinestate <> old.machinestate or new.machinestate_data <> old.machinestate_data then
-	    INSERT INTO person_history (event_timestamp, member_id, machinestate, machinestate_data) VALUES (NOW(), old.id, new.machinestate, new.machinestate_data);
+	if new.machinestate <> old.machinestate or new.machinestate_data <> old.machinestate_data or new.machinestate_expiration_date <> old.machinestate_expiration_date then
+	    INSERT INTO person_history (event_timestamp, member_id, machinestate, machinestate_data, machinestate_expiration_date) VALUES (NOW(), old.id, new.machinestate, new.machinestate_data, new.machinestate_expiration_date);
 	end if;
     end;;
 
 DROP TRIGGER if exists log_person_activity_insert;;
 CREATE TRIGGER log_person_activity_insert AFTER INSERT ON person FOR EACH ROW
-	INSERT INTO person_history (event_timestamp, member_id, machinestate, machinestate_data) VALUES (NOW(), new.id, new.machinestate, new.machinestate_data);;
+	INSERT INTO person_history (event_timestamp, member_id, machinestate, machinestate_data, machinestate_expiration_date) VALUES (NOW(), new.id, new.machinestate, new.machinestate_data, new.machinestate_expiration_date);;
 
 DROP TRIGGER if exists person_auto_entry_date;;
 create trigger person_auto_entry_date before insert on person for each row 
@@ -36,6 +36,9 @@ create trigger person_auto_entry_date before insert on person for each row
 	set new.entrydate=current_date;
 	if new.nickname = '' or new.nickname = NULL then
 	    set new.nickname= concat (new.firstname, '_',new.name); -- Craft an unlikely nickname :-)
+	end if;
+	if new.structuredcomm = '' or new.structuredcomm = NULL then
+	    set new.structuredcomm=(select formatbecomm((select ifnull (max(id), 0) + 1 from person)));
 	end if;
     end;;
 
