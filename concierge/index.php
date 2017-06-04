56add1cf293176fc8bf94c51ef7bba7eb79900d3
frederic <frederic@askarel.be>
2017-06-04 12:38:38 +0200

    Authenticate to LDAP server

--- concierge/index.php
+++ concierge/index.php
@@ -1,6 +1,6 @@
 <?php
 #
-# Concierge for Hackerspace Brussels - Web front-end
+# Concierge for Hackerspace Brussels - Web front-end - Login page
 # (c) 2014 Frederic Pasteleurs <frederic@askarel.be>
 #
 # This program is free software; you can redistribute it and/or modify
@@ -20,47 +20,48 @@
 
 # Load the common stuff (first thing)
 include_once('./lib/commonfunctions.php');
+$MissingUsrText=""; 
+$MissingPwText="";
 
-try 
+function login_page()
 {
-    $MissingUsrText=""; 
-    $MissingPwText="";
-    $dbh = new PDO ("mysql:host=". $CONFIG['dbhostname'] . ";dbname=" . $CONFIG['mydb'] . ";charset=UTF8", $CONFIG['dbuser'], $CONFIG['dbpass']);
-    $dbh->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $dbh->setAttribute (PDO::ATTR_EMULATE_PREPARES, false);
-    $stmt = $dbh->prepare ("select id, passwordhash from person where nickname = :nickname limit 1", array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
+	html_header ('Hello');
+	global $MissingUsrText, $MissingPwText, $CONFIG, $SANITIZED_REQUEST;
+	if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['user'] == "") ) $MissingUsrText="Field cannot be empty" ;
+	if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['pass'] == "") ) $MissingPwText="Field cannot be empty" ;
+	printf ("<div>\n");
+	printf (" <H1>%s Login Form</H1>\n",$CONFIG['CAorgname']);
+	printf (" <FORM Method=\"POST\" Action=\"%s\">\n", $_SERVER['SCRIPT_NAME']);
+	printf ("  Username: <INPUT type=\"text\" size=20 name=\"user\" value=\"%s\">%s<br />\n", $SANITIZED_REQUEST['user'], $MissingUsrText);
+	printf ("  Password: <INPUT type=\"password\" size=20 name=\"pass\">%s<br />\n", $MissingPwText);
+	printf (" <INPUT type=\"submit\" value=\"Login\">\n");
+	printf (" </FORM>\n");
+	printf ("<a HREF=\"forgotpassword.php\">Forgot password ?</a><br />\n");
+	printf ("<a HREF=\"newaccount.php\">Create new account</a><br />\n");
+	printf ("</div>\n");
+}
 
-    // Validate credentials...
-    if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['user'] != "") and ($_REQUEST['pass'] != "") )
-	{
-	    $stmt->execute( array (':nickname' => $SANITIZED_REQUEST['user'] ) );
-	    $result = $stmt->fetchAll();
-
-	    if ( $stmt->rowcount() == 1 )
-	    { // The user exist. Check password...
-		if (password_verify ( $_REQUEST['pass'],$result[0]['passwordhash'] ) )
-		{ // Good password: set person ID into session
-		    $_SESSION['person_ID'] = $result[0]['id'];
-		} else 
-		{
-		    $MissingPwText="Invalid credentials"; // Bad password
-		}
-	    } else
-	    {
-		$MissingPwText="Invalid credentials"; // Specified user does not exist
+// Validate supplied credentials...
+if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['user'] != "") and ($_REQUEST['pass'] != "") )
+    {
+	$rdn="uid=" . $SANITIZED_REQUEST['user'] . ",ou=users," . $CONFIG['ldapdn'];
+	$resu=ldap_bind ($ldapconn, $rdn, $_REQUEST['pass']);
+	if ($resu)
+	    { // Bind operation is successful.
+		$_SESSION['AUTH_RDN'] = $rdn;
+		$_SESSION['AUTH_RDN_PASS'] = $_REQUEST['pass'];
 	    }
-	}
-    $dbh = null;
-}
-catch (PDOException $e) 
-{
-    html_header ('FAIL');
-    printf ("<H1>Database failed: %s</H1><br />\n", $e->GetMessage());
-    die;
-}
+	    else
+	    {
+		$MissingPwText="Wrong credentials supplied.";
+	    }
+	// Debug
+	//printf ("<br />rdn=%s<br />resu=%s<br />ldaperror=%s<br />\n", $rdn, $resu, ldap_error ($ldapconn));
+    }
+
 
 ### Processing done. Act on/display the results
-if (isset($_SESSION['person_ID']))
+if (isset($_SESSION['AUTH_RDN']) and isset($_SESSION['AUTH_RDN_PASS']))
     { // Authenticated bit
 	if ( isset ($_SESSION['redirect']))
 	{
@@ -69,17 +70,7 @@ if (isset($_SESSION['person_ID']))
 	    header ("Location: user_dashboard.php"); // Redirect to user dashboard page if we were not redirected
     } else
     { // Non-authenticated bit: show login page
-	html_header ('Hello');
-	if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['user'] == "") ) $MissingUsrText="Field cannot be empty" ;
-	if ( ($_SERVER['REQUEST_METHOD'] == "POST") and ($_REQUEST['pass'] == "") ) $MissingPwText="Field cannot be empty" ;
-	printf (" <H1>%s Members Login Form</H1>\n", $CONFIG['CAorgname']);
-	printf (" <FORM Method=\"POST\" Action=\"%s\">\n", $_SERVER['SCRIPT_NAME']);
-	printf ("  Username: <INPUT type=\"text\" size=20 name=\"user\" value=\"%s\">%s<br />\n", $SANITIZED_REQUEST['user'], $MissingUsrText);
-	printf ("  Password: <INPUT type=\"password\" size=20 name=\"pass\">%s<br />\n", $MissingPwText);
-	printf (" <INPUT type=\"submit\" value=\"Login\">\n");
-	printf (" </FORM>\n");
-	printf ("<A HREF=\"forgotpassword.php\">Forgot password ?</A><BR />\n");
-	printf ("<A HREF=\"newaccount.php\">Create new account</A><BR />\n");
+	login_page();
     }
-#dumpglobals();
+// dumpglobals();
 ?>
