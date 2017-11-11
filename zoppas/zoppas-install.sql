c9601d74dacdd6cf27dc96973193a86167ff4146
frederic <frederic@askarel.be>
2017-11-11 15:02:09 +0100

    Database reworking: use MySQL instead on CSV file
    >>> INITIAL STAGE <<<

---
+++ zoppas/zoppas-install.sql
@@ -0,0 +1,48 @@
+--
+--	Zoppas - tables definition
+--
+--	(c) 2017 Frederic Pasteleurs <frederic@askarel.be>
+--
+--    This program is free software: you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation, either version 3 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+create table if not exists product (product_id int not null auto_increment primary key, 
+				    description char(255),
+				    shortdescription char(255),
+				    category char(60),
+				    stock_quantity int not null
+				    );
+
+create table if not exists barcodes (id int not null auto_increment primary key, 
+				    product_id int not null,
+				    barcode char(255) unique not null
+				    );
+
+create table if not exists price (id int not null auto_increment primary key, 
+				    product_id int not null,
+				    event_id char(40),
+				    price_validfrom date not null,
+				    price_validto date not null,
+				    price_purchase decimal (15,4) not null,
+				    price_sell decimal (15,4) not null
+				    );
+
+--create table if not exists sale_tickets ( -- id int not null auto_increment primary key, 
+--				    product_id int not null,
+--				    product_quantity int not null,
+--				    product_unit_price decimal (15,4) not null,
+--				    pointofsale char(60)
+--				    );
+
+
