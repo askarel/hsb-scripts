71e828ca3165417a2a2bf363147c3e50d3aadeab
frederic <frederic@askarel.be>
2018-01-29 23:12:47 +0100

    Added: internal accounting stored procedures

--- concierge/sql/functions.sql
+++ concierge/sql/functions.sql
@@ -18,7 +18,8 @@
 --
 DELIMITER ;;
 
-
+-- Randomly generated belgian-style structured communication
+DROP FUNCTION IF EXISTS mkbecomm;;
 CREATE FUNCTION `mkbecomm`() RETURNS char(20) CHARSET latin1
 begin
 set @commbase:=lpad (floor (1 + (rand() * 9999999999 )),10, '0');
@@ -26,6 +27,7 @@ return (select concat ('+++', insert (insert (@commbase, 8, 0, '/'), 4, 0, '/'),
 end ;;
 
 -- Format any number between 0 and 9999999999 into a belgian style structured communication
+DROP FUNCTION IF EXISTS formatbecomm;;
 CREATE FUNCTION formatbecomm (commnumber BIGINT) RETURNS char(20)
 begin
  DECLARE commbase char(10);
@@ -35,6 +37,93 @@ begin
  RETURN (select concat ('+++', insert (insert( commbase, 8, 0, '/'), 4, 0, '/'), commmod , '+++'));
 end ;;
 
+-- Provision empty accounts
+DROP PROCEDURE IF EXISTS intacc_provision;;
+create procedure intacc_provision (commqty INT)
+begin
+ DECLARE x int default 0;
+ while x <= commqty do 
+  insert into internal_accounts (structuredcomm) values ( mkbecomm());
+  set x = x + 1;
+ end while;
+end;;
+
+-- Create the internal account
+-- Return account ID in case of success
+DROP procedure IF EXISTS intacc_create;;
+create procedure intacc_create (ACCNTTYPE char(20), OWNER_DN char(255), REF_DN char(255))
+begin
+ DECLARE INTERNALACCOUNT char(20);
+ set INTERNALACCOUNT:=(select structuredcomm from internal_accounts where account_type is null limit 1);
+ if INTERNALACCOUNT is NULL
+    then 
+	signal SQLSTATE '45000'	SET MESSAGE_TEXT = 'No spare accound IDs found! Run intacc_provision() first!';
+    else
+	update internal_accounts set in_use=1, account_type=ACCNTTYPE, owner_dn=OWNER_DN, created_on=CURRENT_TIMESTAMP, ref_dn=REF_DN where structuredcomm like INTERNALACCOUNT;
+	select INTERNALACCOUNT;
+ END IF;
+end;;
+
+-- Preload an amount on the internal account
+DROP PROCEDURE IF EXISTS intacc_preload;;
+create procedure intacc_preload (accid char(20), POSID char(50), AMOUNT decimal(15,4), CURRENCY char(5), Message char(255))
+begin
+ DECLARE TRANS_ID char(255);
+ DECLARE INTERNALACCOUNT char(20);
+ set INTERNALACCOUNT:=(select structuredcomm from internal_accounts where structuredcomm like accid and in_use=1);
+ if INTERNALACCOUNT is NULL
+    then 
+	signal SQLSTATE '45001'	SET MESSAGE_TEXT = 'Specified internal account ID does not exist.';
+    else
+	SET TRANS_ID:=concat ('INTERNAL/', accid, '/', POSID, '/', sha1(concat (current_timestamp(), Message)) );
+	insert into moneymovements (date_val, date_account, amount, currency, other_account, this_account, message, transaction_id) VALUES 
+                        	    (curdate(), curdate(), concat ('-', abs (AMOUNT)), CURRENCY, POSID, accid, Message, TRANS_ID);
+ END IF;
+end;;
+
+-- Consume an internal account
+DROP PROCEDURE IF EXISTS intacc_consume;;
+create procedure intacc_consume (accid char(20), POSID char(50), AMOUNT2PAY decimal(15,4), CURRENCY char(5), MSG char(255))
+begin
+ DECLARE TRANS_ID char(255);
+ DECLARE INTERNALACCOUNT char(20);
+ DECLARE BALANCE decimal(15,4);
+ set INTERNALACCOUNT:=(select structuredcomm from internal_accounts where structuredcomm like accid and in_use=1);
+ set BALANCE:=(select sum(amount) from moneymovements where this_account like INTERNALACCOUNT);
+ if BALANCE is null 
+	then set BALANCE:=0; 
+ end if;
+ if INTERNALACCOUNT is NULL
+    then 
+	signal SQLSTATE '45001'	SET MESSAGE_TEXT = 'Specified internal account ID does not exist.';
+    else
+    if (AMOUNT2PAY + BALANCE) > 0
+	then 
+	    signal SQLSTATE '45002'	SET MESSAGE_TEXT = 'Cannot pay: not enough money on account.';
+	else
+	    SET TRANS_ID:=concat ('INTERNAL/', accid, '/', POSID, '/', sha1(concat (current_timestamp(), MSG)) );
+	    insert into moneymovements (date_val, date_account, amount, currency, other_account, this_account, message, transaction_id) VALUES 
+                        		(curdate(), curdate(), abs (AMOUNT2PAY), CURRENCY, POSID, accid, MSG, TRANS_ID);
+	end if;
+ END IF;
+end;;
+
+-- Get balance of specified account
+DROP PROCEDURE IF EXISTS intacc_get_balance;;
+create procedure intacc_get_balance (accid char(20))
+begin
+ DECLARE INTERNALACCOUNT char(20);
+ set INTERNALACCOUNT:=(select structuredcomm from internal_accounts where structuredcomm like accid and in_use=1);
+ if INTERNALACCOUNT is NULL
+    then 
+	signal SQLSTATE '45001'	SET MESSAGE_TEXT = 'Specified internal account ID does not exist.';
+    else
+	select abs(sum(amount)), currency from moneymovements where this_account like INTERNALACCOUNT;
+ END IF;
+end;;
+
+-- Transfer between internal accounts
+DROP PROCEDURE IF EXISTS intacc_transfer;;
 
 
 DELIMITER ;
